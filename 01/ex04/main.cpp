4832a8b35812079944d592af47ed32a0ab2f272a
Charaf3334 <toujana2015@gmail.com>
2025-07-15 16:51:29 +0100

    deleted a comment

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -1,25 +1,5 @@
 #include "Sed.hpp"
 
-// static std::string replacing(std::string& line, std::string& search, std::string& replace)
-// {
-//     std::string result;
-//     size_t i = 0;
-
-//     while (1) 
-//     {
-//         size_t found = line.find(search, i);
-//         if (found == std::string::npos)
-//         {
-//             result += line.substr(i);
-//             break ;
-//         }
-//         result += line.substr(i, found - i) + replace;
-//         i = found + search.length();
-//     }
-//     return result;
-// }
-
-
 int main(int ac, char **av)
 {
     if (ac != 4)
