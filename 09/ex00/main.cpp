ee1c59174f78c3f45bf0ce839c38b8f7e2278f3c
Charaf3334 <toujana2015@gmail.com>
2025-10-14 13:17:19 +0100

    update main()

--- 09/ex00/main.cpp
+++ 09/ex00/main.cpp
@@ -2,6 +2,7 @@
 
 int main(int ac, const char **av)
 {
+    BitcoinExchange *btc = NULL;
     try
     {
         if (ac != 2)
@@ -9,13 +10,18 @@ int main(int ac, const char **av)
             std::cerr << "Error: number of args is not valid." << std::endl;
             return 1;
         }
-        BitcoinExchange btc(av[1]);
-        btc.readDatabase("data.csv");
-        btc.checkFileAndPrint();
+        btc = new BitcoinExchange(av[1]);
+        btc->readDatabase("data.csv");
+        btc->checkFileAndPrint();
+
+        delete btc;
+        btc = NULL;
     }
     catch (const std::exception &e)
     {
         std::cerr << e.what() << std::endl;
+        if (btc)
+            delete btc;
         return 1;
     }
     return 0;
